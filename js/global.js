e02ba4b75ea21c0db3036e197f8e9ba24cc283b9
lsvih <hnhao78@163.com>
2016-06-20 18:47:58 +0800

    Optimize code;Delete useless code;Regulate object's name.

--- js/global.js
+++ js/global.js
@@ -1,12 +1,15 @@
 function startload() {
 	document.getElementById("load").style.display = "block";
-	
+
 }
 
 function stopload() {
 	document.getElementById("load").style.display = "none";
 }
-
+if (localStorage.userinfo) {
+	var strAccessToken = JSON.parse(localStorage.userinfo).access_token;
+	var nUserId = JSON.parse(localStorage.userinfo).id;
+}
 var serverurl = "http://vruanzhuang.cn";
 var apiurl = "http://vruanzhuang.cn/smart_matching/api/web/"
 var appurl = serverurl + "/vrz/api/web";
